6c2db1d550fe0e3eea9486ea13a4cf66e4f0a69a
ld1024 <575784835@qq.com>
2020-05-09 22:26:36 +0800

    init

--- server/controller/common/index.js
+++ server/controller/common/index.js
@@ -1,27 +1,2 @@
 
 const pool = require('../mysql')
-
-
-class poolFn {
-
-  constructor(obj){
-    this.table = obj.table
-    this.query = obj.query
-    this.pageInfo = obj.pageInfo
-  }
-
-  getInfoById(){
-    const sql = `SELECT * FROM ? WHERE id = ?`
-    pool.query(sql,this.table,this.id, (err, res)=>{
-      pool.release()
-      if(err) {
-        return err
-      }
-      return res
-    })
-  }
-}
-
-module.exports = {
-  queryBy: new poolFn()
-}
